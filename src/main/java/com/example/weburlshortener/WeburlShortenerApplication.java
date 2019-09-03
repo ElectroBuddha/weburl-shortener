1e02f946c7feaf91b2f267af6f5480a2cd654b91
Vedran Rašić <vedran.rasic@outlook.com>
2019-09-03 06:31:37 +0200

    Initial commit

---
+++ src/main/java/com/example/weburlshortener/WeburlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weburlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeburlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeburlShortenerApplication.class, args);
+	}
+
+}
